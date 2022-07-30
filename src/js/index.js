e2052567099f4273a9704385aa78613e5e63f913
jiho1996 <jiho.urssu@gmail.com>
2022-07-30 17:57:50 +0900

    feat : 빈 값 예외 처리

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,30 @@ function App(){
         e.preventDefault();
     });
     //메뉴 이름 받기.
+    console.log($("#espresso-menu-name").value === "")
     $("#espresso-menu-name")
     .addEventListener("keypress", (e) =>{
-        // 
+        console.log($("#espresso-menu-name").value,"and",e.key);
+        if (e.key !== "Enter"){
+            return;
+        }
+        // 의문 1. 아래 if문에서 만약 뭔가를 쳤다가 다 지우면 $("#espresso-menu-name").value === ""
+        // 되지않나? -> 일단 addeventlistener MDN찾기.
+       
+        if ($("#espresso-menu-name").value === ""){
+            alert("값을 입력해주세요.");
+            return;
+        }
+
+         // 의문 2. 왜 빈값 처리를 아래 if e.key == enter문 사이에 안넣고 위에 넣는가..?
+         // 최대한 depth를 줄이기위해서..?
+
         if (e.key === "Enter"){
             const coffeeName = $('#espresso-menu-name').value;
-            
+            // if (coffeeName === ""){
+            //     alert("값을 입력해 주세요.");
+            //     return;
+            // }
             const coffeeMenuTemplate = (coffeeName) => {
                 return `
                     <li class="menu-list-item d-flex items-center py-2">
@@ -38,10 +56,18 @@ function App(){
             const menuCount = $("#espresso-menu-list").querySelectorAll("li").length
             $(".menu-count").innerText = `총 ${menuCount}개`
             $('#espresso-menu-name').value = "";
+            
         };
-
-
-        
+       
     });
+
+   
 }
+// function App(){
+//     document
+//     .querySelector("#espresso-menu-name")
+//     .addEventListener("keypress", (e) =>{
+//         console.log(e);
+//     });
+// };
 App();
