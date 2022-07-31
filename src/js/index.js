c03942c0ccabaae8ebedec4598beae0d4dff12c1
jiho1996 <jiho.urssu@gmail.com>
2022-07-31 18:55:40 +0900

    feat : prompt 기본 값에 현재 값 넣기

--- src/js/index.js
+++ src/js/index.js
@@ -59,8 +59,8 @@ function App(){
 
     $("#espresso-menu-list").addEventListener("click", (e) => {
         if (e.target.classList.contains("menu-edit-button")){
-           
-            const updatedMenuName = prompt("변경할 이름을 입력해주세요.");
+            const $innerMenuName = e.target.closest("li").querySelector(".menu-name")
+            const updatedMenuName = prompt("변경할 이름을 입력해주세요.", $innerMenuName.innerText);
             
         
         };
