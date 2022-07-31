3690b2e799b8672be8b7dcd19c66866691cf37f7
jiho1996 <jiho.urssu@gmail.com>
2022-07-31 18:51:16 +0900

    feat : 메뉴 수정시 브라우저에서 제공하는 `prompt` 인터페이스를 활용.

--- src/js/index.js
+++ src/js/index.js
@@ -60,6 +60,8 @@ function App(){
     $("#espresso-menu-list").addEventListener("click", (e) => {
         if (e.target.classList.contains("menu-edit-button")){
             
+            const updatedMenuName = prompt("변경할 이름을 입력해주세요.");
+            
         
         };
     })
