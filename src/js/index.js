05073a670a55858698ec9d78316b3244c238b048
jiho1996 <jiho.urssu@gmail.com>
2022-07-31 18:56:46 +0900

    feat :수정한 메뉴 반영 구현

--- src/js/index.js
+++ src/js/index.js
@@ -61,7 +61,7 @@ function App(){
         if (e.target.classList.contains("menu-edit-button")){
             const $innerMenuName = e.target.closest("li").querySelector(".menu-name")
             const updatedMenuName = prompt("변경할 이름을 입력해주세요.", $innerMenuName.innerText);
-            
+            $innerMenuName.innerText = updatedMenuName;
         
         };
     })
